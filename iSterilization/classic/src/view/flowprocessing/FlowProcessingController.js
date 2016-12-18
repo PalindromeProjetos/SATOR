4eeb9d2a1ba200c3b5e43013d0145086970dfff8
SamuelOliveira <samuel.dasilva@iatec.com>
2016-12-18 04:20:30 -0200

    Movement Fix

--- iSterilization/classic/src/view/flowprocessing/FlowProcessingController.js
+++ iSterilization/classic/src/view/flowprocessing/FlowProcessingController.js
@@ -234,11 +234,6 @@ Ext.define( 'iSterilization.view.flowprocessing.FlowProcessingController', {
                                 Smart.Msg.showToast(resultSet.getMessage(), 'error');
                                 return false;
                             }
-
-                            // if( data.get('movementtype') == '001' ) {
-                            //     var url = 'business/Calls/Quick/DispensingReport.php?id={0}';
-                            //     window.open(Ext.String.format(url,data.get('id')));
-                            // }
                         }
                     });
 
