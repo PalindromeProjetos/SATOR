a58d72926e15dc33a3b7bdb4d0c0229be768ebab
SamuelOliveira <samuel.dasilva@iatec.com>
2016-12-25 01:38:14 -0200

    Release Consulta Material

--- iSterilization/classic/src/view/flowprocessing/protocol/Call_SATOR_MOVIMENTO_OF.js
+++ iSterilization/classic/src/view/flowprocessing/protocol/Call_SATOR_MOVIMENTO_OF.js
@@ -79,6 +79,26 @@ Ext.define( 'iSterilization.view.flowprocessing.protocol.Call_SATOR_MOVIMENTO_OF
                                 flex: 2,
                                 name: 'countitems',
                                 style : { 'text-align': 'right', 'color': 'rgb(173, 20, 87)' }
+                            }, {
+                                width: 30,
+                                height: 30,
+                                xtype: 'component',
+                                html: '<div class="smart-btn-header" style="padding-left: 7px;"><i class="fa fa-times"></i></div>',
+                                style: {
+                                    cursor: 'pointer',
+                                    fontSize: '20px;',
+                                    borderRadius: '50%',
+                                    backgroundColor:'rgb(246, 246, 246);'
+                                },
+                                listeners: {
+                                    render: function(c){
+                                        c.getEl().on({
+                                            click: function() {
+                                                me.close();
+                                            }
+                                        });
+                                    }
+                                }
                             }
                         ]
                     }, {
